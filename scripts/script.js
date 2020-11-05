ab6492e384adcc2dbaf1c86c35d750d99fd14215
Alejandro Marco <alexmarco@gmx.co.uk>
2020-11-05 07:49:33 +0000

    comment out js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    var helloButton = document.querySelector("#hello-button");
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-        //helloButton.classList.replace("is-dark","is-light");
-    });
-})();
+// (function() {
+//     var burger = document.querySelector('.burger');
+//     var menu = document.querySelector('#'+burger.dataset.target);
+//     var helloButton = document.querySelector("#hello-button");
+//     burger.addEventListener('click', function() {
+//         burger.classList.toggle('is-active');
+//         menu.classList.toggle('is-active');
+//         //helloButton.classList.replace("is-dark","is-light");
+//     });
+// })();
